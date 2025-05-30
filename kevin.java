3a39777cba48173b1e67d8d0034fb77ea4a78dcd
kmeng9025 <85904902+kmeng9025@users.noreply.github.com>
2025-05-30 09:43:43 -0400

    push

--- kevin.java
+++ kevin.java
@@ -1,6 +1,24 @@
-/**DO NOT MODIFY THIS FILE */
 import java.io.*;
-import java.util.*;
+public class kevin {
+    public static void main(String[] args) throws Exception {
+        IOReader io = new IOReader();
+        int n = io.readInt();
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            String line = io.readLine();
+            String[] parts = line.split(" ");
+            int a = Integer.parseInt(parts[0]);
+            int b = Integer.parseInt(parts[1]);
+            int c = Integer.parseInt(parts[2]);
+            if((a + b + c) >= 2){
+                result ++;
+            }
+        }
+        io.println(result);
+        io.close();
+    }
+}
+
 class IOReader{
 
     private BufferedReader br;
